0fbfd40815f12212d41ad493a54f57adca351a1d
tomdoeslinux <tomdoeslinux@protonmail.com>
2023-12-13 18:59:16 +1300

    Add loading fallback when loading trips.

--- frontend/src/pages/trip-page/TripPage.tsx
+++ frontend/src/pages/trip-page/TripPage.tsx
@@ -1,9 +1,53 @@
-import { Box, Flex, Text, Grid, GridItem, Heading } from '@chakra-ui/react'
+import { Box, Flex, Text, Grid, GridItem, Heading, Spinner } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
-import { MdCalendarMonth, MdLocationPin } from 'react-icons/md'
+import { MdLocationPin } from 'react-icons/md'
 import { API, Trip } from 'src/api'
 import DayItem from 'src/pages/trip-page/components/DayItem'
 import Map from 'src/pages/trip-page/components/Map'
+
+interface TripHeaderProps {
+    trip: Trip
+}
+
+function TripHeader(props: TripHeaderProps) {
+    const imgPath = API.getImgPath(props.trip.photo)
+
+    return (
+        <Flex
+            width='100%'
+            backgroundImage={imgPath}
+            backgroundSize='cover'
+            height='400px'
+        >
+            <Flex
+                width='100%'
+                padding='32px'
+                background='linear-gradient(to bottom, transparent 50%, black 100%)'
+            >
+                <Flex
+                    marginTop='auto'
+                    color='white'
+                    flexDirection='column'
+                    gap='8px'
+                >
+                    <Flex gap='8px' alignItems='center'>
+                        <MdLocationPin />
+                        <Text>{props.trip.destination.name}</Text>
+                    </Flex>
+                    <Heading marginTop='auto' as='h1'>{props.trip.name}</Heading>
+                </Flex>
+            </Flex>
+        </Flex>
+    )
+}
+
+function TripLoading() {
+    return (
+        <Flex width='100vw' height='100vh' alignItems='center' justifyContent='center'>
+            <Spinner />
+        </Flex>
+    )
+}
 
 interface TripPageProps {
     tripId: number
@@ -22,60 +66,38 @@ export default function TripPage(props: TripPageProps) {
     }, [])
 
     return (
-        <Grid templateColumns='repeat(2, 1fr)' width='100%' height='100vh'>
-            <GridItem
-                gridColumn={1}
-                gap='8px'
-                display='flex'
-                flexDirection='column'
-            >
-                {trip && (
-                    <>
-                        <Flex 
-                            width='100%' 
-                            backgroundImage={API.getImgPath(trip.photo)} 
-                            backgroundSize='cover' 
-                            height='400px'
-                        >
-                            <Flex 
-                                width='100%' 
-                                padding='32px' 
-                                background='linear-gradient(to bottom, transparent 50%, black 100%)'
-                            >
-                                <Flex 
-                                    marginTop='auto' 
-                                    color='white' 
-                                    flexDirection='column' 
-                                    gap='8px'
-                                >
-                                    <Flex gap='8px' alignItems='center'>
-                                        <MdLocationPin />
-                                        <Text>{trip.destination.name}</Text>
-                                    </Flex>
-                                    <Heading marginTop='auto' as='h1'>{trip.name}</Heading>          
-                                </Flex>
-                            </Flex>
-                        </Flex>
+        <>
+            {!trip && <TripLoading />}
 
-                        <Flex padding='24px' flexDirection='column' gap='8px'>
-                            {trip.days.map((day, index) => (
-                                <DayItem key={index} day={day} />
-                            ))}
-                        </Flex>
-                    </>
-                )}
-            </GridItem>
-
-            <GridItem position='relative' gridColumn={2} background='gray'>
-                <Box position='fixed' width='100%' height='100%'>
+            {trip && (
+                <Grid templateColumns='repeat(2, 1fr)' width='100%' height='100vh'>
                     {trip && (
-                        <Map 
-                            startingLocation={trip.destination} 
-                            destinations={trip.days.map((tripDay) => tripDay.activities.map((activity) => activity.destination)).flat()} 
-                        />
+                        <>
+                            <GridItem
+                                gridColumn={1}
+                                gap='8px'
+                                display='flex'
+                                flexDirection='column'
+                            >
+                                <TripHeader trip={trip} />
+
+                                <Flex padding='24px' flexDirection='column' gap='8px'>
+                                    {trip.days.map((day, index) => (
+                                        <DayItem key={index} day={day} />
+                                    ))}
+                                </Flex>
+                            </GridItem>
+
+                            <GridItem position='relative' gridColumn={2} background='gray'>
+                                <Map 
+                                    startingLocation={trip.destination} 
+                                    destinations={trip.days.map((tripDay) => tripDay.activities.map((activity) => activity.destination)).flat()} 
+                                />
+                            </GridItem>
+                        </>
                     )}
-                </Box>
-            </GridItem>
-        </Grid>
+                </Grid>
+            )}
+        </>
     )
 }
