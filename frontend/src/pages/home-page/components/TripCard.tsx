1ffc6bd5814ed9b0319de9f1d5803706e8354672
tom <tomdoeslinux@protonmail.com>
2023-12-28 18:58:22 +1300

    Image resizing fixes.

--- frontend/src/pages/home-page/components/TripCard.tsx
+++ frontend/src/pages/home-page/components/TripCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, Heading, IconButton } from "@chakra-ui/react"
+import {Card, CardBody, CardHeader, Heading, IconButton} from "@chakra-ui/react"
 import { API, Trip } from "src/api"
 import TripCardOptions from "src/pages/home-page/components/TripCardOptions"
 
@@ -10,12 +10,13 @@ interface TripCardProps {
 export default function TripCard(props: TripCardProps) {
     return (
         <Card 
-            flexDirection='column' 
-            minHeight='300px' 
+            flexDirection='column'
             onClick={() => props.onClick(props.trip.id)}
-            backgroundImage={API.getImgPath(props.trip.photo)}
-            backgroundSize='cover'
         >
+            <CardBody padding='0'>
+                <img style={{ maxHeight: '300px', width: '100%', objectFit: 'cover', aspectRatio: 1000 / 750 }} src={API.getImgPath(props.trip.photo)} alt={''}/>
+            </CardBody>
+
             <CardHeader height='60px' display='flex' flexDirection='row' alignItems='center'>
                 <Heading as='h3' size='sm' fontFamily='Noto Sans' fontWeight='normal'>{props.trip.name}</Heading>
 
